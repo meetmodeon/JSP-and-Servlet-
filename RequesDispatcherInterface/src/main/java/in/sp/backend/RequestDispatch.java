654dc985846c8e17f2ac8b8ffb20fe65e3fc1b79
Meet_Yadav <120273933+meetmodeon@users.noreply.github.com>
2024-03-09 01:08:28 +0545

    RequestDispatcherInterface Simple use program

---
+++ RequesDispatcherInterface/src/main/java/in/sp/backend/RequestDispatch.java
@@ -0,0 +1,36 @@
+package in.sp.backend;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/submit")
+public class RequestDispatch extends HttpServlet{
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String myemail=req.getParameter("email1");
+		String myname=req.getParameter("name1");
+		String mypass=req.getParameter("pass1");
+		String mylogin=req.getParameter("Login");
+		PrintWriter out=resp.getWriter();
+		if(myemail.equals("meetyadav@gmail.com")&& mypass.equals("123456789")) {
+			req.setAttribute("name_key",myname);
+			
+			RequestDispatcher rd=req.getRequestDispatcher("/profile.jsp");
+			rd.forward(req,resp);
+		}
+		else {
+			resp.setContentType("text/html");
+			out.print("<h5 style='color:red'>Email id and password didnt matched</h5>");
+			RequestDispatcher rd=req.getRequestDispatcher("/index.html");
+			rd.include(req, resp);
+		}
+	}
+
+}
